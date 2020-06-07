e2ee7d849f141e2c79a30703e0c8e78c281fdfca
Jim Kubicek <jkubicek@gmail.com>
2020-06-06 23:34:51 -0700

    Filtering now works!

--- jkViz.py
+++ jkViz.py
@@ -2,21 +2,24 @@ import click
 import subprocess
 import os
 
-from flask import Flask, g, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 app = Flask(__name__)
+app.secret_key = "soTjK_UgaHcK8HM4nBTwFv.UvzMcNxyPXKnH_TWYoZ@Dc.*PdKxVs.kaCTtJ-UXZ"
 
 @app.route('/')
 def index():
-    g.filename = os.environ["JQ_FILENAME"]
-
-    g.filter = '.'
-    
-    output = subprocess.check_output(["jq", g.filter, g.filename]).decode()
-    return render_template("index.html", json=output, filename=g.filename, filter=g.filter)
+    filename = os.environ["JQ_FILENAME"]
+    jq_filter = session.get("filter", ".")
+    print(f"jq_filter: {jq_filter}")
+    try:
+        output = subprocess.check_output(["jq", jq_filter, filename]).decode()
+    except subprocess.CalledProcessError as jq_err:
+        output = jq_err.output.decode()
+    return render_template("index.html", json=output, filename=filename, filter=jq_filter)
 
 @app.route('/update-filter', methods=['POST'])
 def update_filter():
-    g.filter = request.form["jq_filter"]
+    session["filter"] = request.form["jq_filter"]
     return redirect(url_for("index"))
 
 @click.command()

7f5052ac76564080662bb8f6247f9578698719cb
Jim Kubicek <jkubicek@gmail.com>
2020-06-06 22:42:10 -0700

    I have some templates, and they work!

--- jkViz.py
+++ jkViz.py
@@ -2,15 +2,15 @@ import click
 import subprocess
 import os
 
-from flask import Flask, g
+from flask import Flask, g, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def root_route():
     g.filename = os.environ["JQ_FILENAME"]
     query = '.'
-    output = subprocess.check_output(["jq", query, g.filename])
-    return output.decode()
+    output = subprocess.check_output(["jq", query, g.filename]).decode()
+    return render_template("index.html", json=output)
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))

318f51abe2f616d290c1897b8c603efc508ec97b
Jim Kubicek <jkubicek@gmail.com>
2020-06-06 23:02:40 -0700

    Redirecs adn form submissions now work

--- jkViz.py
+++ jkViz.py
@@ -2,15 +2,22 @@ import click
 import subprocess
 import os
 
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
-def root_route():
+def index():
     g.filename = os.environ["JQ_FILENAME"]
-    query = '.'
-    output = subprocess.check_output(["jq", query, g.filename]).decode()
-    return render_template("index.html", json=output)
+
+    g.filter = '.'
+    
+    output = subprocess.check_output(["jq", g.filter, g.filename]).decode()
+    return render_template("index.html", json=output, filename=g.filename, filter=g.filter)
+
+@app.route('/update-filter', methods=['POST'])
+def update_filter():
+    g.filter = request.form["jq_filter"]
+    return redirect(url_for("index"))
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True))

b3679daf97c91c1e56eb7b0a3b85182156f21c60
Jim Kubicek <jkubicek@gmail.com>
2020-08-24 18:42:52 -0700

    Fix the github URL

--- src/jqviz/__init__.py
+++ src/jqviz/__init__.py
@@ -2,7 +2,7 @@ __version__ = "0.0.2"
 
 __title__ = "jqviz"
 __description__ = "Visualize your jq commands"
-__url__ = "https://github.com/jkubicek/jkviz"
+__url__ = "https://github.com/jkubicek/jqviz"
 __uri__ = __url__
 __doc__ = __description__ + " <" + __uri__ + ">"
 
